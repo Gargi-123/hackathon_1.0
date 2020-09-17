eaa1699b7bce0ab188574e4d83706bef948f2ac2
Gargi-123 <dasgargi100@gmail.com>
2020-09-17 11:30:03 +0530

    first commit

---
+++ task/my-react-app/src/components/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Index extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+    render(){
+       
+    }
+}
